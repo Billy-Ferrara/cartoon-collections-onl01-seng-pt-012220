9b0f6500f9e35ed2d0df8a33b730d65521f5d1b0
Billy Ferrara <willfira717@gmail.com>
2020-01-31 18:18:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,15 +7,17 @@ end
 roll_call_dwarves(dwarves)
 end
 
+
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 def summon_captain_planet(array)
-  planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+  
   planeteer_array = []
   i = 0 
   while i < array.length 
     planeteer_array << array[i].capitalize + "!"
     i += 1 
   end 
-  planeteer_array
+  planeteer_array(planeteer_calls)
 end
 
 def long_planeteer_calls(array) 
