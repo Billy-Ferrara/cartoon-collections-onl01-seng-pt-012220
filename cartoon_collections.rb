b1db496799df09b6078788f5486f593fee2c7bb4
Billy Ferrara <willfira717@gmail.com>
2020-01-31 18:17:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,20 +8,26 @@ roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(array)
-  nu_array = []
+  planeteer_array = []
   i = 0 
   while i < array.length 
-    nu_array << array[i].capitalize + "!"
+    planeteer_array << array[i].capitalize + "!"
     i += 1 
   end 
-  nu_array
+  planeteer_array
 end
 
-summon_captain_planet(planeteer_calls)
-
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array) 
+  i = 0 
+  if  array.any? {|i| i.length > 4}
+    return true
+  else 
+    return false
+  i = i + 1
+  end 
 end
+
+long_planeteer_calls(call_screams)
 
 def find_the_cheese# code an argument here
   # the array below is here to help
